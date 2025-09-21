c6ea812537c29c63835133c8fc4a77d26dfeb180
uk030621 <123797901+uk030621@users.noreply.github.com>
2025-09-21 20:53:10 +0100

    Changed header bg and text colour

--- components/IncidentTable.js
+++ components/IncidentTable.js
@@ -52,7 +52,7 @@ export default function IncidentTable() {
       <div className="overflow-x-auto">
         <table className="w-full border-collapse border rounded shadow text-sm">
           <thead>
-            <tr className="bg-gray-200">
+            <tr className="bg-white text-black">
               <th className="p-2 border">Name</th>
               <th className="p-2 border">Email</th>
               <th className="p-2 border">Flat No.</th>
